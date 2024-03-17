ba56b4fd5c0a4fcad2376d65236a973eff473e4d
anjana732 <anjanasingh1257@gmail.com>
2024-03-17 14:45:12 +0530

    Click event is added

--- events/src/components/ListItem.jsx
+++ events/src/components/ListItem.jsx
@@ -2,10 +2,14 @@ import { Button } from 'bootstrap'
 import React from 'react'
 import style from './ListItem.module.css'
 
+function clickEvent(item){
+    console.log(`${item} is being buy`)
+}
+
 function ListItem({item}) {
   return (
     <>
-        <li class="list-group-item">{item} <button className={style.btnn}>Buy</button></li>
+        <li class="list-group-item">{item} <button className={style.btnn} onClick={() => {clickEvent(item)}}>Buy</button></li>
         
     </>
   )
