5fe9e61c59ee58420ae277b7bb5efc1fc2a11511
anjana732 <anjanasingh1257@gmail.com>
2024-03-28 15:15:15 +0530

    resolved newTask issue

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -6,18 +6,28 @@ import { useState } from 'react'
 
 function App() {
   const todo = [{task:"Buy Milk",
-                  date:"12-03-23"},{task:"Go to gym", date: "12-03-23"}]
+                  dueDate:"12-03-23"},{task:"Go to gym", dueDate: "12-03-23"}]
 
-  const [task, setTask] = useState(todo);
+  const [newtask, setNewTask] = useState(todo);
 
-    const handleNewToDo = (toDoName, toDoDueDate) => {
+    /*const handleNewToDo = (toDoName, toDoDueDate) => {
       console.log(`todoName: ${toDoName} , toDoDueDate: ${toDoDueDate}`)
-    }
+      const newToDoItem = [...newtask, {task: toDoName, date: toDoDueDate}];
+      console.log(newToDoItem);
+      setNewTask(newToDoItem);
+    }*/
+    const handleNewToDo = (toDoName, toDoDueDate) => {
+      console.log(`todoName: ${toDoName} , toDoDueDate: ${toDoDueDate}`);
+      const newToDoItem = [...newtask, { task: toDoName, dueDate: toDoDueDate }]; // Use 'dueDate' instead of 'date'
+      console.log(newToDoItem);
+      setNewTask(newToDoItem);
+    };
+    
   return (
     <>
       <Header></Header>
       <ToDoInput onNewToDo={handleNewToDo}></ToDoInput>
-      <ToDos todos={todo}></ToDos>
+      <ToDos todos={newtask}></ToDos>
     </>
   )
 }
