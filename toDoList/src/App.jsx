f6d2c64de1bbbff7e11205937d6d5b6fb7df093e
anjana732 <anjanasingh1257@gmail.com>
2024-03-28 13:22:15 +0530

    Added functionality to todo

--- toDoList/src/App.jsx
+++ toDoList/src/App.jsx
@@ -2,15 +2,21 @@ import './App.css'
 import ToDoInput from './components/ToDoInput'
 import Header from './components/Header'
 import ToDos from './components/ToDos'
+import { useState } from 'react'
 
 function App() {
   const todo = [{task:"Buy Milk",
                   date:"12-03-23"},{task:"Go to gym", date: "12-03-23"}]
 
+  const [task, setTask] = useState(todo);
+
+    const handleNewToDo = (toDoName, toDoDueDate) => {
+      console.log(`todoName: ${toDoName} , toDoDueDate: ${toDoDueDate}`)
+    }
   return (
     <>
       <Header></Header>
-      <ToDoInput></ToDoInput>
+      <ToDoInput onNewToDo={handleNewToDo}></ToDoInput>
       <ToDos todos={todo}></ToDos>
     </>
   )
--- toDoList/src/components/ToDoInput.jsx
+++ toDoList/src/components/ToDoInput.jsx
@@ -1,11 +1,29 @@
+import { useState } from 'react'
 import style from './ToDoInput.module.css'
 
-function ToDoInput(){
+function ToDoInput({onNewToDo}){
+    const [toDoName,setToDoName] = useState();
+    const [toDoDate, setToDoDate] = useState();
+
+    function onNameChange(e){
+        console.log(e.target.value);
+        setToDoName(e.target.value);
+    }
+    function onDateChange(e){
+        console.log(e.target.value);
+        setToDoDate(e.target.value);
+    }
+
+    const handleAddButtonClick = () => {
+        onNewToDo(toDoName,toDoDate)
+        setToDoName("");
+        setToDoDate("");
+    }
     return(
         <>
-            <input type="text"></input>
-            <input type="date"></input>
-            <button>Add</button>
+            <input type="text" value={toDoName} onChange={onNameChange}></input>
+            <input type="date" value={toDoDate} onChange={onDateChange}></input>
+            <button onClick={()=>{handleAddButtonClick()}}>Add</button>
         </>
     )
 }
