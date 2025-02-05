7091ec9bad294123337b7519187b3a41043693fa
anjana732 <anjanasingh1257@gmail.com>
2025-02-05 11:15:43 +0530

    Add redux-toolkit project

--- contextToDo/src/components/index.js
+++ contextToDo/src/components/index.js
@@ -1,4 +1,7 @@
-import TodoForm from "./ToDoFrom";
+// import TodoForm from "./ToDoFrom";
+// import TodoItem from "./TodoItem";
+
+import TodoForm from "./TodoFrom";
 import TodoItem from "./TodoItem";
 
 export {TodoForm, TodoItem} 
--- theme_switcher/src/App.jsx
+++ theme_switcher/src/App.jsx
@@ -4,11 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className="text-blue-200">Hello</h1>
+      
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
     </>
   )
 }
