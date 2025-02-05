5866f6a234c25ba178f9fc5a9bd9de2ac0eadc2b
anjana732 <anjanasingh1257@gmail.com>
2025-02-05 12:42:01 +0530

    Add Todos functionality

--- reduxToolkit/src/components/Todos.jsx
+++ reduxToolkit/src/components/Todos.jsx
@@ -1,7 +1,19 @@
-export function Todos(){
+import { useSelector, useDispatch } from "react-redux"
+import { removeTodo } from "../features/todo/storeSlice";
+
+function Todos(){
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch();
     return(
         <>
-        
+            {
+                todos.map((todo)=> (
+                <li key={todo.id}>{todo.text} 
+                <button onClick={() => dispatch(removeTodo(todo.id))}> Delete</button>
+                </li>))
+            }
         </>
     )
 }
+
+export default Todos
