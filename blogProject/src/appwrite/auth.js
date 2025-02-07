b0fa40f79f8665e8569eebfb1291e55172ac9d48
anjana732 <anjanasingh1257@gmail.com>
2025-02-07 17:34:44 +0530

    Add Auth Service

--- blogProject/src/appwrite/auth.js
+++ blogProject/src/appwrite/auth.js
@@ -16,7 +16,7 @@ export class AuthService{
         try {
            const userAccount =  await this.account.create(ID.unique,email, password, name);
            if (userAccount) {
-                return userAccount;
+                return this.login({email, password});
            } else {
                 return userAccount;
            }
@@ -27,9 +27,25 @@ export class AuthService{
 
     async login({email, password}){
         try {
-            return await this.account.createEmailSession(email, password)
+            return await this.account.createEmailPasswordSession(email, password)
         } catch (error) {
             throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();  
+        } catch (error) {
+            throw error;
         }
     }
 }
