198b45d309a02e45ccd1894dcc79e146ca94deea
anjana732 <anjanasingh1257@gmail.com>
2025-02-17 23:34:49 +0530

    Add General button

---
+++ blogProject/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React  from "react";
+
+function Button({children,
+                type = 'button',
+                bgColor = 'bg-blue-600',
+                textColor = 'text-white',
+                className = '',
+                ...props
+}){
+    return(
+        <button>{children}</button>
+    )
+}
